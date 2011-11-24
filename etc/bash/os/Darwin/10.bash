2f3631a1a1a534eb7f2705e5d6f4d2eb2b28ab19
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2011-11-24 22:13:47 +0000

    Have colours in iTerm

---
+++ etc/bash/os/Darwin/10.bash
@@ -0,0 +1,6 @@
+# github:bruntonspall:home:etc/bash/os/Darwin/10.bash
+# -*- Mode: Bash; tab-width: 4; indent-tabs-mode: nil; -*-
+#
+#       See ~/share/home/setup.markdown for more information.
+
+export CLICOLOR=1
