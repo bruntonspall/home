e5c1fb30e699f99572fe6e8866c56b3e588c8c45
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2013-06-26 15:49:18 +0100

    Add old binary scripts

---
+++ bin/wow.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Setup to automatically extract the latest undermine journal data
+file=/tmp/TheUndermineJournal.zip
+
+if [[ ! -f $file || $(stat -f %c $file) -le $(( `date +%s` - 7200 )) ]]; then 
+curl "https://theunderminejournal.com/TheUndermineJournal.zip?key=okqzVApi4bAbzNW8ndiy6oj1Uv5sZTpE&realms=EUH-Stormrage" -o$file
+unzip -o $file -d /Volumes/BACKUPS/World\ of\ Warcraft/Interface/Addons/
+fi
+
+cd /Volumes/BACKUPS/World\ of\ Warcraft/Interface/Addons/ElvUI.git
+git checkout -- .
+git pull
+
+/Volumes/BACKUPS/World\ of\ Warcraft/World\ of\ Warcraft-64.app/Contents/MacOS/World\ of\ Warcraft-64
