6b13cb178d280a92e96dad7986bb66b6f349d320
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2014-10-21 07:43:09 +0100

    Update local binaries

--- bin/wow.sh
+++ bin/wow.sh
@@ -4,11 +4,11 @@ file=/tmp/TheUndermineJournal.zip
 
 if [[ ! -f $file || $(stat -f %c $file) -le $(( `date +%s` - 7200 )) ]]; then 
 curl "https://theunderminejournal.com/TheUndermineJournal.zip?key=okqzVApi4bAbzNW8ndiy6oj1Uv5sZTpE&realms=EUH-Stormrage" -o$file
-unzip -o $file -d /Volumes/BACKUPS/World\ of\ Warcraft/Interface/Addons/
+unzip -o $file -d /Volumes/icklecat/World\ of\ Warcraft/Interface/Addons/
 fi
 
-cd /Volumes/BACKUPS/World\ of\ Warcraft/Interface/Addons/ElvUI.git
+cd /Volumes/icklecat/World\ of\ Warcraft/Interface/Addons/ElvUI.git
 git checkout -- .
 git pull
 
-/Volumes/BACKUPS/World\ of\ Warcraft/World\ of\ Warcraft-64.app/Contents/MacOS/World\ of\ Warcraft-64
+/Volumes/icklecat/World\ of\ Warcraft/World\ of\ Warcraft-64.app/Contents/MacOS/World\ of\ Warcraft-64
